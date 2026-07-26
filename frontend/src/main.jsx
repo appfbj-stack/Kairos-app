586dff57db83fdd9dc43c1f96b32c5bcb817c574
fernandojaborges-sys <fernandojaborges@gmail.com>
2026-07-26 15:06:38 -0300

    fix: limpa cache SW na inicializacao + headers cache nginx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './styles/globals.css'
 import App from './App.jsx'
 
+if ('serviceWorker' in navigator) {
+  caches.keys().then(nomes => Promise.all(nomes.map(n => caches.delete(n))))
+  navigator.serviceWorker.getRegistrations().then(regs =>
+    regs.forEach(reg => reg.unregister())
+  )
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
